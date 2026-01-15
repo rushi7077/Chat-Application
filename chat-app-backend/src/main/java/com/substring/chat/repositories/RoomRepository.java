f6b838516c7255e8a9607ac026b66b0b3e38bd82
Rushi Baravkar <121597406+rushi7077@users.noreply.github.com>
2026-01-15 19:15:22 +0530

    Delete chat-app-backend directory

--- chat-app-backend/src/main/java/com/substring/chat/repositories/RoomRepository.java
+++
@@ -1,8 +0,0 @@
-package com.substring.chat.repositories;
-
-import com.substring.chat.entities.Room;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface RoomRepository extends MongoRepository<Room,String> {
-    Room findByRoomId(String roomId);
-}
